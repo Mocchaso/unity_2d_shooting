bdcf62fbc746314f9bdd4c3b7775c4a7155f5cfc
Mocchaso <saka.ksftn2718@gmail.com>
2019-06-18 01:45:25 +0900

    現在のチュートリアルプロジェクト
    12. Waveを5個にする、スコアの実装まで終了

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    // アニメーションが終了したときに呼び出されるメソッド(動作にはUnity上で別途設定が必要)
+    void OnAnimationFinish()
+    {
+        Destroy(gameObject);
+    }
+}
